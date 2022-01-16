2f23533ce9916fbc99aefd1020e74b34626754b7
Melih Ömer Kamar <97173932+MelihOmer@users.noreply.github.com>
2022-01-16 22:31:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Csharp_Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           System.Console.WriteLine(Gunler.Pazartesi);
+           System.Console.WriteLine((int)Gunler.Cumartesi);
+
+            int Sicaklik = 32;
+            if (Sicaklik <=(int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("Dışarıya Çıkmak İçin Havanın Biraz Daha Isınmasını Bekleyelim");
+            }
+            else if (Sicaklik >= (int)HavaDurumu.Sıcak)
+            {
+                System.Console.WriteLine("Dışarıya Çıkmak İçin Çok Sıcak Bir Gün");
+            }
+            else if (Sicaklik >=(int)HavaDurumu.Normal && Sicaklik <(int)HavaDurumu.CokSicak)
+            {
+                System.Console.WriteLine("Hadi Dışarı Çıkalım");
+            }
+
+
+        }    
+    }
+    enum Gunler
+    {
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk=5,
+
+        Normal=20,
+
+        Sıcak = 25,
+
+        CokSicak = 30
+    }
+    
+}
